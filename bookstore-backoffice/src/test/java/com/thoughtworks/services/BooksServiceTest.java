bf9ea28218c9dcdcad3bee681e6be47cc93b046a
CooTWer <zryu@thoughtworks.com>
2016-08-02 17:27:43 +0800

    complete get books success unit test

--- bookstore-backoffice/src/test/java/com/thoughtworks/services/BooksServiceTest.java
+++ bookstore-backoffice/src/test/java/com/thoughtworks/services/BooksServiceTest.java
@@ -36,5 +36,46 @@ public class BooksServiceTest {
         assertEquals( new ResponseEntity("'msg':'error message'", INTERNAL_SERVER_ERROR), responseEntity);
     }
 
+    @Test
+    public void ShouldGetBooksFromBookServiceSuccess() throws Exception {
+        //given
+        XmlParse xmlParseMockedService = mock(XmlParse.class);
+        RestTemplate restTemplateMockedService = mock(RestTemplate.class);
+        booksService.xmlParse = xmlParseMockedService;
+        booksService.restTemplate = restTemplateMockedService;
+        when(xmlParseMockedService.pathParse("")).thenReturn("mock service address");
+        when(restTemplateMockedService.getForEntity("mock service address",String.class)).thenReturn(new ResponseEntity("'msg':'success'",HttpStatus.OK));
 
+        //when
+        ResponseEntity responseEntity = booksService.getBooks();
+
+        //then
+        assertEquals( new ResponseEntity("'msg':'success'",HttpStatus.OK),responseEntity);
+    }
+//
+////    @Test
+////    public void ShouldEditBooksFromBookService() throws Exception{
+////        XmlParse xmlParseMockedService = mock(XmlParse.class);
+////        RestTemplate restTemplateMockedService = mock(RestTemplate.class);
+////        when(restTemplateMockedService.put("http://localhost:8080/MockService/books/isbn",new String("hello"))).thenReturn(new ResponseEntity<>("success", HttpStatus.OK));
+////
+////    }
+//
+//    @Test
+//    public void ShouldDeleteBookFromBookServiceSuccess(){
+//        HttpEntity<?> request = new HttpEntity<Object>("1,2,3");
+//        RestTemplate restTemplateMockedService = mock(RestTemplate.class);
+//        when(restTemplateMockedService.exchange("http://localhost:8080/MockService/books", HttpMethod.DELETE, request , String.class)).thenReturn(new ResponseEntity<>("'msg': 'Book deleted'", HttpStatus.OK));
+//        assertEquals(new ResponseEntity<>("'msg': 'Book deleted'", HttpStatus.OK),restTemplateMockedService.exchange("http://localhost:8080/MockService/books", HttpMethod.DELETE, request , String.class));
+//
+//    }
+//
+//    @Test
+//    public void ShouldDeleteBookFromBookServiceFail(){
+//        HttpEntity<?> request = new HttpEntity<Object>("1,2,3");
+//        RestTemplate restTemplateMockedService = mock(RestTemplate.class);
+//        when(restTemplateMockedService.exchange("http://localhost:8080/MockService/books", HttpMethod.DELETE, request , String.class)).thenReturn(new ResponseEntity<>("'msg': 'username or password is error'", HttpStatus.UNAUTHORIZED));
+//        assertEquals(new ResponseEntity<>("'msg': 'username or password is error'", HttpStatus.UNAUTHORIZED),restTemplateMockedService.exchange("http://localhost:8080/MockService/books", HttpMethod.DELETE, request , String.class));
+//
+//    }
 }
